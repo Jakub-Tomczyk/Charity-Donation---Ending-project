785cab8597e248f0769bd5dc70bcb92e8f97ac0d
Jakub Tomczyk <tomczyk.jakub@ymail.com>
2020-06-16 15:31:16 +0200

    Create DonationService for CRUD

---
+++ src/main/java/pl/coderslab/charity/service/DonationService.java
@@ -0,0 +1,13 @@
+package pl.coderslab.charity.service;
+
+import org.springframework.stereotype.Service;
+import pl.coderslab.charity.repository.DonationRepository;
+
+@Service
+public class DonationService {
+    private final DonationRepository donationRepository;
+
+    public DonationService(DonationRepository donationRepository) {
+        this.donationRepository = donationRepository;
+    }
+}
