57d88c15c14d658527b25a29005146929de9fce4
Jakub Tomczyk <tomczyk.jakub@ymail.com>
2020-06-19 11:02:25 +0200

    Add Institution Converter

---
+++ src/main/java/pl/coderslab/charity/converter/InstitutionConverter.java
@@ -0,0 +1,18 @@
+package pl.coderslab.charity.converter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import pl.coderslab.charity.entity.Institution;
+import pl.coderslab.charity.repository.InstitutionRepository;
+
+@Component
+public class InstitutionConverter  implements Converter<String, Institution> {
+
+    @Autowired
+    private InstitutionRepository institutionRepository;
+    @Override
+    public Institution convert(String id) {
+        return institutionRepository.findOneById(Long.parseLong(id));
+    }
+}
