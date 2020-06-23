e399dca93f633b2d53bc4a1a7a087bd4ce4e50b3
Jakub Tomczyk <tomczyk.jakub@ymail.com>
2020-06-23 12:02:55 +0200

    Made Spring Data User Details Service with metods.

---
+++ src/main/java/pl/coderslab/charity/service/SpringDataUserDetailsService.java
@@ -0,0 +1,32 @@
+package pl.coderslab.charity.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import pl.coderslab.charity.entity.Role;
+import pl.coderslab.charity.model.CurrentUser;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SpringDataUserDetailsService  implements UserDetailsService {
+    private UserService userService;
+
+    @Autowired
+    public void setUserRepository(UserService userService) {this.userService = userService;}
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        User user = userService.findByUserName(username);
+        if (user == null) {throw new UsernameNotFoundException(username);}
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        Set<Role> roles = user.getRoles();
+        roles.forEach(role -> grantedAuthorities.add(new SimpleGrantedAuthority(role.getName())));
+        return new CurrentUser(user.getUsername(), user.getPassword(), grantedAuthorities, user);
+    }
+}
