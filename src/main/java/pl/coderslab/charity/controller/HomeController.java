535f87e8db0263000235eaa6feb62010259a2598
Jakub Tomczyk <tomczyk.jakub@ymail.com>
2020-06-27 16:08:44 +0200

    Delete not using security Principal.

--- src/main/java/pl/coderslab/charity/controller/HomeController.java
+++ src/main/java/pl/coderslab/charity/controller/HomeController.java
@@ -8,12 +8,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.charity.service.DonationService;
 import pl.coderslab.charity.service.InstitutionService;
 
+import java.security.Principal;
+
 
 @Controller
 public class HomeController {
 
     private final InstitutionService institutionService;
     private final DonationService donationService;
+
 
     public HomeController(InstitutionService institutionService, DonationService donationService) {
         this.institutionService = institutionService;
